75330ebfc6dbf2b47fd652bbc9c82d4ae0543846
Thorben Bender <bender.thorben0@gmail.com>
2019-08-12 11:15:02 +0200

    functionality to create a new user

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,24 @@ class App extends Component {
   // State
   state = {
     // show the current component
-    view: 'Create'
+    view: 'Create',
+    user: []
   }
 
   changeView = view => {
     // will set the state of view based on the view argument
     this.setState({view: view})
   }
+  // takes in a user as an argument
+  createUser = user => {
+    // it will create a new array and fill in every user from the previous state and add the new user to the end
+    this.setState(st => ({user: [...st.user, user]}))
+  }
   render() {
     return (
       <div className="App">
         {/* If view is equal to Create it will render the create component */}
-        {this.state.view === 'Create' && <Create changeView={this.changeView}/>}
+        {this.state.view === 'Create' && <Create changeView={this.changeView} createUser={this.createUser} />}
         {/* If view is equal to ViewAndUpdate it will render the ViewAndUpdate component */}
         {this.state.view === 'ViewAndUpdate' && <ViewAndUpdate changeView={this.changeView}/>}
       </div>
--- client/src/Components/Create/Create.js
+++ client/src/Components/Create/Create.js
@@ -5,7 +5,8 @@ export default class Create extends Component {
     state = {
         name: '',
         email: '',
-        gender: ''
+        gender: '',
+        error: null
     }
 
     // takes event as an argument
@@ -13,17 +14,42 @@ export default class Create extends Component {
         // will set the state from the item with the name of the event target to the value of the event target
         this.setState({[e.target.name]: e.target.value})
     }
+
+    submitInput = e => {
+        // will prevent reload
+        e.preventDefault();
+        // check if every input field is filled out
+        if (!this.state.name || !this.state.email || !this.state.gender) {
+            // if not it will set the state from error to missing credentials
+            this.setState({error: 'Missing credentials'})
+            // and return to null to end the function
+            return null
+        }
+        // else
+        // create a user object with the values from state
+        let user = {
+            name: this.state.name,
+            email: this.state.email,
+            gender: this.state.gender
+        }
+        // call the function from the parent element with user object
+        this.props.createUser(user)
+        // change the view to ViewAndUpdate
+        this.props.changeView('ViewAndUpdate')
+    }
     render() {
         return (
             <div>
-                <input name='name' value={this.state.name} placeholder="Name..." onChange={this.handleInput} />
-                <input name='email' value={this.state.email} placeholder="Email..." onChange={this.handleInput} />
+                <input name='name' type='text' value={this.state.name} placeholder="Name..." onChange={this.handleInput} />
+                <input name='email' type='email' value={this.state.email} placeholder="Email..." onChange={this.handleInput} />
                 <select name='gender' onClick={this.handleInput}>
                     <option defaultValue='select a gender'>Select a gender</option>
                     <option value='male'>Male</option>
                     <option value='female'>Female</option>
                     <option value='other'>other</option>
                 </select>
+                {this.state.error && <p>{this.state.error}</p>}
+                <button onClick={this.submitInput}>Submit</button>
             </div>
         )
     }
