219253bedc6c106ed6e426bdc2b88d80a5ab32c9
Thorben Bender <bender.thorben0@gmail.com>
2019-08-12 10:55:49 +0200

    created layout for create component | created functionality for the create component

--- client/src/Components/Create/Create.js
+++ client/src/Components/Create/Create.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Create extends Component {
+    // state to store data from the input fields
+    state = {
+        name: '',
+        email: '',
+        gender: ''
+    }
+
+    // takes event as an argument
+    handleInput = e => {
+        // will set the state from the item with the name of the event target to the value of the event target
+        this.setState({[e.target.name]: e.target.value})
+    }
     render() {
         return (
             <div>
-                Create User Profile
+                <input name='name' value={this.state.name} placeholder="Name..." onChange={this.handleInput} />
+                <input name='email' value={this.state.email} placeholder="Email..." onChange={this.handleInput} />
+                <select name='gender' onClick={this.handleInput}>
+                    <option defaultValue='select a gender'>Select a gender</option>
+                    <option value='male'>Male</option>
+                    <option value='female'>Female</option>
+                    <option value='other'>other</option>
+                </select>
             </div>
         )
     }
