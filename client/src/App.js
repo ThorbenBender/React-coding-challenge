2f4f6baaebc6830c48bdd7e5232046d67b3764fc
Thorben Bender <bender.thorben0@gmail.com>
2019-08-12 10:37:37 +0200

    basic layout created | functionality created to switch between views

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Clean Up</p>
-    </div>
-  );
+// Created Components
+import Create from './Components/Create/Create'
+
+import ViewAndUpdate from './Components/ViewAndUpdate/ViewAndUpdate'
+
+class App extends Component {
+  state = {
+    view: 'Create'
+  }
+
+  changeView = view => {
+    this.setState({view: view})
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.view === 'Create' && <Create changeView={this.changeView}/>}
+        {this.state.view === 'ViewAndUpdate' && <ViewAndUpdate changeView={this.changeView}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Components/ViewAndUpdate/ViewAndUpdate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ViewAndUpdate extends Component {
+    render() {
+        return (
+            <div>
+                <p>ViewAndUpdate</p>
+            </div>
+        )
+    }
+}
